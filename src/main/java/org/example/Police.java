cfcce76685e29794b4de61bc5385cdfabc1a6f8d
Adam9870 <adam.pasniewski@tm1.edu.pl>
2025-12-16 10:52:55 +0100

    Delete src directory

--- src/main/java/org/example/Police.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Police extends Movable {
-    Police(double posX, double posY) {
-        super(posX, posY);
-    }
-}
--- src/main/java/org/example/SkinColor.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public enum SkinColor {
-    WHITE_PALE,
-    WHITE,
-    TANNED,
-    MEXICAN,
-    BLACK
-}
