158659d5c7d0d79ee2e1320a70495f224568d819
Adam9870 <adam.pasniewski@tm1.edu.pl>
2026-01-10 21:19:43 +0100

    Create Difficulty.java

---
+++ src/main/java/org/example/Difficulty.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Poziom trudności gry
+ * Każdy poziom ma przypisany kolor wizualny
+ */
+
+public enum Difficulty {
+    EASY,
+    NORMAL,
+    HARD,
+    INSANE
+}
